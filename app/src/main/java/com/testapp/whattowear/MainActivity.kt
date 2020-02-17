a3ef65dc325852088ba2a8425a84ef024a037f53
mvproject <awsom7@gmail.com>
2020-02-17 12:25:21 +0200

    change min sdk support add MainViewModel add MainFragment and connect with MainViewModel show fragment in MainActivity

--- app/src/main/java/com/testapp/whattowear/MainActivity.kt
+++ app/src/main/java/com/testapp/whattowear/MainActivity.kt
@@ -2,11 +2,16 @@ package com.testapp.whattowear
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.testapp.whattowear.ui.fragment.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().replace(R.id.hostFragment,
+            MainFragment()
+        ).commit()
     }
 }
