e46f4a4bd6aed53f3d896acdc247d38612da3456
mvproject <awsom7@gmail.com>
2020-02-18 11:41:51 +0200

    show error status message in Toast

--- app/src/main/java/com/testapp/whattowear/ui/fragment/MainFragment.kt
+++ app/src/main/java/com/testapp/whattowear/ui/fragment/MainFragment.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.gms.common.api.Status
@@ -55,7 +56,7 @@ class MainFragment : Fragment() {
 
         autoComplete.setOnPlaceSelectedListener(object : PlaceSelectionListener{
             override fun onError(status: Status) {
-                // TODO maybe some error message
+                Toast.makeText(context,status.statusMessage,Toast.LENGTH_SHORT).show()
             }
 
             override fun onPlaceSelected(place: Place) {
