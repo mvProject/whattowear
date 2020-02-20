e4627670bb61e67d58288526e23afdd52709a9e1
mvproject <awsom7@gmail.com>
2020-02-20 13:01:46 +0200

    weather api service created for fetching data internet permission add to manifest

---
+++ app/src/main/java/com/testapp/whattowear/network/WeatherApiService.kt
@@ -0,0 +1,28 @@
+package com.testapp.whattowear.network
+
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.testapp.whattowear.utils.getApiInterceptor
+import com.testapp.whattowear.utils.getLoggingInterceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class WeatherApiService {
+
+    fun initApi() : DarkSkyApi {
+
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(getLoggingInterceptor())
+            .addInterceptor(getApiInterceptor())
+            .build()
+        return Retrofit.Builder().apply {
+            baseUrl(BASE_URL)
+            addCallAdapterFactory(CoroutineCallAdapterFactory())
+            addConverterFactory(GsonConverterFactory.create())
+            client(okHttpClient)
+        }.build().create(DarkSkyApi::class.java)
+    }
+    companion object {
+        const val BASE_URL = "https://api.darksky.net/"
+    }
+}
