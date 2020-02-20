577550996dabd7bd82d9bce679087567a3e4a3f2
mvproject <awsom7@gmail.com>
2020-02-20 10:19:09 +0200

    fixed accessing to viewmodel change date converting to seconds

--- app/src/main/java/com/testapp/whattowear/dialog/TripDatePickerDialog.kt
+++ app/src/main/java/com/testapp/whattowear/dialog/TripDatePickerDialog.kt
@@ -6,16 +6,22 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.DatePicker
 import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.ViewModelProvider
+import com.testapp.whattowear.ui.viewmodel.MainViewModel
 import java.util.*
 
 class TripDatePickerDialog : DialogFragment(), DatePickerDialog.OnDateSetListener {
 
+    private lateinit var mainViewModel: MainViewModel
     private val START_DATE_DIALOG = "StartDateDialog"
     private val END_DATE_DIALOG = "EndDateDialog"
 
     private val c: Calendar = Calendar.getInstance()
 
+
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         val year = c.get(Calendar.YEAR)
         val month = c.get(Calendar.MONTH)
         val day = c.get(Calendar.DAY_OF_MONTH)
@@ -23,6 +29,7 @@ class TripDatePickerDialog : DialogFragment(), DatePickerDialog.OnDateSetListene
     }
 
     override fun onDateSet(view: DatePicker, year: Int, month: Int, day: Int) {
+        c.set(year,month,day,12,0)
         when (tag){
             START_DATE_DIALOG -> {
                 Log.d("Wear","start date selected")
@@ -31,7 +38,6 @@ class TripDatePickerDialog : DialogFragment(), DatePickerDialog.OnDateSetListene
                 Log.d("Wear","end date selected")
             }
         }
-        c.set(year,month,day,12,0)
-        Log.d("Wear","$tag date selected ${c.timeInMillis}")
+        Log.d("Wear","$tag date selected ${c.timeInMillis / 1000}")
     }
 }
