560bb4f09a882ee3af921e99cd11741af61aed14
mvproject <awsom7@gmail.com>
2020-02-26 10:13:59 +0200

    gradle.xml add to gitignore test for expected list pass

--- app/src/test/java/com/testapp/whattowear/utils/DataRangeUtilsTest.kt
+++ app/src/test/java/com/testapp/whattowear/utils/DataRangeUtilsTest.kt
@@ -5,8 +5,8 @@ import org.junit.Test
 import org.junit.Assert.*
 
 class DataRangeUtilsTest {
-    private val testStartDate = 1582641234L
-    private val testEndDate = 1582986834L
+    private val testStartDate = 1582704449000L
+    private val testEndDate = 1582963649000L
     @Test
     fun getDataRangeForTrip_Is_Not_Null() {
         val result = getDataRangeForTrip(testStartDate,testEndDate)
@@ -22,4 +22,11 @@ class DataRangeUtilsTest {
         val result = getDataRangeForTrip(0L,0L)
         assertEquals(null,result)
     }
+//
+    @Test
+    fun getDataRangeForTrip_Expected_List() {
+        val expected = listOf<Long>(1582704449,1582790849,1582877249,1582963649)
+        val result = getDataRangeForTrip(testStartDate,testEndDate)
+        assertEquals(expected,result)
+}
 }
