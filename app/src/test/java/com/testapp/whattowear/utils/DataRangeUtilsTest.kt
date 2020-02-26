bef54527a61dafe6c0e23ef7ee444c964498bc0a
mvproject <awsom7@gmail.com>
2020-02-26 12:15:43 +0200

    remove slaches code format fix

--- app/src/test/java/com/testapp/whattowear/utils/DataRangeUtilsTest.kt
+++ app/src/test/java/com/testapp/whattowear/utils/DataRangeUtilsTest.kt
@@ -9,24 +9,26 @@ class DataRangeUtilsTest {
     private val testEndDate = 1582963649000L
     @Test
     fun getDataRangeForTrip_Is_Not_Null() {
-        val result = getDataRangeForTrip(testStartDate,testEndDate)
-        assertNotEquals(null,result)
+        val result = getDataRangeForTrip(testStartDate, testEndDate)
+        assertNotEquals(null, result)
     }
+
     @Test
     fun getDataRangeForTrip_Is_Null() {
-        val result = getDataRangeForTrip(testEndDate,testStartDate)
-        assertEquals(null,result)
+        val result = getDataRangeForTrip(testEndDate, testStartDate)
+        assertEquals(null, result)
     }
+
     @Test
     fun getDataRangeForTrip_With_Zero_Params() {
-        val result = getDataRangeForTrip(0L,0L)
-        assertEquals(null,result)
+        val result = getDataRangeForTrip(0L, 0L)
+        assertEquals(null, result)
     }
-//
+
     @Test
     fun getDataRangeForTrip_Expected_List() {
-        val expected = listOf<Long>(1582704449,1582790849,1582877249,1582963649)
-        val result = getDataRangeForTrip(testStartDate,testEndDate)
-        assertEquals(expected,result)
-}
+        val expected = listOf<Long>(1582704449, 1582790849, 1582877249, 1582963649)
+        val result = getDataRangeForTrip(testStartDate, testEndDate)
+        assertEquals(expected, result)
+    }
 }
