c7874ee7a62443f90b993c7c49a7878b15562931
mvproject <awsom7@gmail.com>
2020-02-28 11:53:35 +0200

    code format

--- app/src/main/java/com/testapp/whattowear/utils/DataRangeUtils.kt
+++ app/src/main/java/com/testapp/whattowear/utils/DataRangeUtils.kt
@@ -4,23 +4,22 @@ import android.text.format.DateUtils
 import java.util.concurrent.TimeUnit
 
 fun getDataRangeForTrip(startDate: Long?, endDate: Long?): List<Long>? {
-    return when(isProperDataRangeSelected(startDate,endDate)){
-        false-> null
-        true->{
+    return when (isProperDataRangeSelected(startDate, endDate)) {
+        false -> null
+        true -> {
             val dataRange = mutableListOf<Long>()
             for (item in startDate!!..endDate!! step DateUtils.DAY_IN_MILLIS)
                 dataRange.add(TimeUnit.MILLISECONDS.toSeconds(item))
             dataRange
         }
     }
-
 }
 
-fun isProperDataRangeSelected(startDate: Long?, endDate: Long?) : Boolean{
+fun isProperDataRangeSelected(startDate: Long?, endDate: Long?): Boolean {
     return when {
         (startDate == null) or (endDate == null) -> false
         (startDate!! <= 0L) or (endDate!! <= 0L) -> false
-        startDate>endDate -> false
+        startDate > endDate -> false
         else -> true
     }
 }
