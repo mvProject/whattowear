dea47c68e8cab40d8abf28f0490aa44ea707fcb2
mvproject <awsom7@gmail.com>
2020-02-28 11:00:39 +0200

    refactor binding adapters for proper date

--- app/src/main/java/com/testapp/whattowear/utils/BindingUtils.kt
+++ app/src/main/java/com/testapp/whattowear/utils/BindingUtils.kt
@@ -6,12 +6,16 @@ import com.testapp.whattowear.R
 
 @BindingAdapter("viewEndTripText")
 fun getProperTextForEndTextView(view: TextView, value: Long) {
-    view.text =
-        if (value > 0) value.convertDateToReadableFormat() else view.resources.getString(R.string.btn_end_date_name)
+    view.text = when(isDateConvertible(value)){
+        true-> value.convertDateToReadableFormat()
+        false-> view.resources.getString(R.string.btn_end_date_name)
+    }
 }
 
 @BindingAdapter("viewStartTripText")
 fun getProperTextForStartTextView(view: TextView, value: Long) {
-    view.text =
-        if (value > 0) value.convertDateToReadableFormat() else view.resources.getString(R.string.btn_start_date_name)
+    view.text = when(isDateConvertible(value)){
+        true-> value.convertDateToReadableFormat()
+        false-> view.resources.getString(R.string.btn_start_date_name)
+    }
 }
