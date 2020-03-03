a392492bb81ff6ed28f740b8f7048470ec4ff401
mvproject <awsom7@gmail.com>
2020-03-03 11:30:02 +0200

    repository logic prepare

--- app/src/main/java/com/testapp/whattowear/data/wear/IWhatToWearRepository.kt
+++ app/src/main/java/com/testapp/whattowear/data/wear/IWhatToWearRepository.kt
@@ -1,10 +1,12 @@
 package com.testapp.whattowear.data.wear
 
 import com.testapp.whattowear.data.PlaceTrip
+import com.testapp.whattowear.data.WeatherData
 import com.testapp.whattowear.data.wear.model.WearItem
 import com.testapp.whattowear.data.wear.model.WeatherTemp
 
 interface IWhatToWearRepository {
+    fun getWeatherForecastForSelectedPlace(place: PlaceTrip): List<WeatherData>
     fun getWearsAvailableForSelect(condition: List<WeatherTemp>): List<WearItem>
     fun getLastSaveSelectedPlace(): PlaceTrip
     fun getLastSaveSelectedDateRange(): List<Long>
--- app/src/main/java/com/testapp/whattowear/data/wear/WhatToWearRepository.kt
+++ app/src/main/java/com/testapp/whattowear/data/wear/WhatToWearRepository.kt
@@ -1,11 +1,16 @@
 package com.testapp.whattowear.data.wear
 
 import com.testapp.whattowear.data.PlaceTrip
+import com.testapp.whattowear.data.WeatherData
 import com.testapp.whattowear.data.wear.model.WearItem
 import com.testapp.whattowear.data.wear.model.WeatherTemp
 
 class WhatToWearRepository :
     IWhatToWearRepository {
+    override fun getWeatherForecastForSelectedPlace(place: PlaceTrip): List<WeatherData> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
     override fun getWearsAvailableForSelect(condition: List<WeatherTemp>): List<WearItem> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
