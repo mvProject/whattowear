4d68fdc190fcdf9d5f4135b1efe13960561c8125
mvproject <awsom7@gmail.com>
2020-03-04 09:01:50 +0200

    weather repository test pass

---
+++ app/src/test/java/com/kinectpro/whattowear/utils/WeatherRangeRepositoryTest.kt
@@ -0,0 +1,28 @@
+package com.kinectpro.whattowear.utils
+
+import com.kinectpro.whattowear.data.wear.WeatherRangeRepository
+import com.kinectpro.whattowear.data.wear.model.TempSummary
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class WeatherRangeRepositoryTest {
+
+    private val tempList = listOf(-10.5f, -5.5f, 0.5f, 5.5f, 10.5f)
+    private val emptyList = listOf<Float>()
+
+    @Test
+    fun getTempMinMaxValue_LIST() {
+        assertEquals(TempSummary(-10.5f, 10.5f), WeatherRangeRepository().getTempMinMaxValue(tempList))
+    }
+
+    @Test
+    fun getTempMinMaxValue_EMPTY_LIST() {
+        assertEquals(null, WeatherRangeRepository().getTempMinMaxValue(emptyList))
+    }
+
+    @Test
+    fun getTempMinMaxValue_NULL() {
+        assertEquals(null, WeatherRangeRepository().getTempMinMaxValue(null))
+    }
+}
