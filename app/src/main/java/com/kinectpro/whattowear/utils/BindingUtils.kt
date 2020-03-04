0cd5a2a94284f814ae6260be2a6bf99cabe4f7fa
mvproject <awsom7@gmail.com>
2020-03-04 13:16:43 +0200

    add picasso library prepare image binding adapter

--- app/src/main/java/com/kinectpro/whattowear/utils/BindingUtils.kt
+++ app/src/main/java/com/kinectpro/whattowear/utils/BindingUtils.kt
@@ -1,7 +1,10 @@
 package com.kinectpro.whattowear.utils
 
+import android.graphics.drawable.Drawable
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.google.android.material.textview.MaterialTextView
+import com.squareup.picasso.Picasso
 
 /**
  * Binding adapter for Date TextView's
@@ -11,12 +14,25 @@ import com.google.android.material.textview.MaterialTextView
  */
 @BindingAdapter(value = ["tripDate", "placeholderText"])
 fun getProperTextForStartTextView(
-        view: MaterialTextView,
-        timestamp: Long,
-        placeholderText: String
+    view: MaterialTextView,
+    timestamp: Long,
+    placeholderText: String
 ) {
     view.text = when (isDateConvertible(timestamp)) {
         true -> timestamp.convertDateToReadableFormat()
         false -> placeholderText
     }
+}
+
+/**
+ * Binding adapter to state icon
+ * @param view type of view which adapter can be binded
+ * @param url drawable according state type
+ * @param placeHolder default drawable for view
+ */
+@BindingAdapter(value = ["stateDrawable", "placeholder"], requireAll = false)
+fun loadImage(
+    view: ImageView, url: String?, placeHolder: Drawable
+) {
+    Picasso.get().load(url).placeholder(placeHolder).into(view)
 }
