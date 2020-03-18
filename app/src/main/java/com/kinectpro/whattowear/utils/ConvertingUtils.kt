991871b69df5827161e24bf8cc9a00ab6552a03d
mvproject <awsom7@gmail.com>
2020-03-18 15:52:25 +0200

    refactor temperature value description show

--- app/src/main/java/com/kinectpro/whattowear/utils/ConvertingUtils.kt
+++ app/src/main/java/com/kinectpro/whattowear/utils/ConvertingUtils.kt
@@ -120,11 +120,14 @@ fun List<WeatherData>.getWeatherStatesUniqueAsList(): List<String> {
  * Extension to convert list of dates to specified short date united spanned string
  * @return single spanned string value
  */
-fun List<Long>.convertToShortDateFormatSpannedString(backgroundColor : Int,foregroundColor : Int): SpannedString {
+fun List<Long>.convertToShortDateFormatSpannedString(
+    backgroundColor: Int,
+    foregroundColor: Int
+): SpannedString {
     return this.joinToSpannedString(" ") {
         TimeUnit.SECONDS.toMillis(it).convertDateToReadableFormat(
             STATE_DATE_READABLE_PATTERN
-        ).convertToRoundedBackgroundSpannable(backgroundColor,foregroundColor)
+        ).convertToRoundedBackgroundSpannable(backgroundColor, foregroundColor)
     }
 }
 
@@ -132,25 +135,55 @@ fun List<Long>.convertToShortDateFormatSpannedString(backgroundColor : Int,foreg
  * Extension for joining spanned strings
  * @return SpannableStringBuilder
  */
-private fun <T> Iterable<T>.joinToSpannedString(separator: CharSequence = ", ", prefix: CharSequence = "", postfix: CharSequence = "", limit: Int = -1, truncated: CharSequence = "...", transform: ((T) -> CharSequence)? = null): SpannedString {
-    return SpannedString(joinTo(SpannableStringBuilder(), separator, prefix, postfix, limit, truncated, transform))
+private fun <T> Iterable<T>.joinToSpannedString(
+    separator: CharSequence = ", ",
+    prefix: CharSequence = "",
+    postfix: CharSequence = "",
+    limit: Int = -1,
+    truncated: CharSequence = "...",
+    transform: ((T) -> CharSequence)? = null
+): SpannedString {
+    return SpannedString(
+        joinTo(
+            SpannableStringBuilder(),
+            separator,
+            prefix,
+            postfix,
+            limit,
+            truncated,
+            transform
+        )
+    )
 }
 
 /**
  * Extension convert temp summary to specified string
  * @return single string value
  */
-fun TempSummary.convertToReadableRange(context : Context): String {
-    return String.format(context.getString(R.string.temperature_max_value_description),maxValue.roundToInt()) + "\n" + String.format(context.getString(R.string.temperature_min_value_description),minValue.roundToInt())
+fun TempSummary.convertToReadableRange(context: Context): String {
+    return String.format(
+        context.getString(R.string.temperature_value_description),
+        maxValue.roundToInt(), minValue.roundToInt()
+    )
 }
 
 /**
  * Extension converting any single string to spannable with predefined decoration
  */
-fun String.convertToRoundedBackgroundSpannable(backgroundColor : Int,foregroundColor : Int) : SpannableString{
+fun String.convertToRoundedBackgroundSpannable(
+    backgroundColor: Int,
+    foregroundColor: Int
+): SpannableString {
     val span = SpannableString("$this ")
     span.setSpan(StyleSpan(BOLD), 0, span.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-    span.setSpan(RoundedBackgroundSpan(backgroundColor,foregroundColor,BACKGROUND_CORNER_RADIUS,HORIZONTAL_PADDING,
-        VERTICAL_PADDING), 0, span.length-1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+    span.setSpan(
+        RoundedBackgroundSpan(
+            backgroundColor,
+            foregroundColor,
+            BACKGROUND_CORNER_RADIUS,
+            HORIZONTAL_PADDING,
+            VERTICAL_PADDING
+        ), 0, span.length - 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+    )
     return span
 }
