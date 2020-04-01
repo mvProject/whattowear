d60e81e803fa2821c55774e97b54899fbe9c6da7
mvproject <awsom7@gmail.com>
2020-04-01 13:05:50 +0300

    trip weather condition refactor

--- app/src/main/java/com/kinectpro/whattowear/data/storage/ICache.kt
+++ app/src/main/java/com/kinectpro/whattowear/data/storage/ICache.kt
@@ -1,7 +1,6 @@
 package com.kinectpro.whattowear.data.storage
 
 import com.kinectpro.whattowear.data.model.location.PlaceTrip
-import com.kinectpro.whattowear.data.model.wear.WearItem
 
 interface ICache {
     fun getLastSelectedPlace(): PlaceTrip?
