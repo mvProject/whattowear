563cfb0a1ed32a0efa79bc170598261807b62948
mvproject <awsom7@gmail.com>
2020-04-28 13:21:23 +0300

    add connection pool for client

--- app/src/main/java/com/kinectpro/whattowear/network/api/DarkSkyWeatherApiService.kt
+++ app/src/main/java/com/kinectpro/whattowear/network/api/DarkSkyWeatherApiService.kt
@@ -4,12 +4,13 @@ import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterF
 import com.kinectpro.whattowear.network.endpoint.DarkSkyEndPoint
 import com.kinectpro.whattowear.utils.getLoggingInterceptor
 import com.kinectpro.whattowear.utils.getRequestSettingsInterceptor
+import okhttp3.ConnectionPool
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
-const val REQUEST_TIMEOUT = 30L
+const val REQUEST_TIMEOUT = 10L
 
 class DarkSkyWeatherApiService {
 
@@ -19,6 +20,7 @@ class DarkSkyWeatherApiService {
             .connectTimeout(REQUEST_TIMEOUT, TimeUnit.SECONDS)
             .addInterceptor(getRequestSettingsInterceptor())
             .addInterceptor(getLoggingInterceptor())
+            .connectionPool(ConnectionPool(0, 15, TimeUnit.SECONDS))
             .build()
         return Retrofit.Builder().apply {
             baseUrl(BASE_URL)
